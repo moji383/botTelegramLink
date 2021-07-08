b0edfdb43c0715943c8f8d7ab28914ed6588c8b8
moji383 <34909254+moji383@users.noreply.github.com>
2021-07-08 22:48:21 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+API_KEY = os.environ['API_KEY']
+import telebot
+import requests
+import string
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['hello'])
+def greet(message):
+  bot.reply_to(message, "Hey! Hows it going?")
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+  
+  file_id = message.photo[2].file_id
+  file_info = bot.get_file(file_id)
+  print(file_info)
+  file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(API_KEY, file_info.file_path))
+  open(file_id+'.png','wb').write(file.content)
+  
+@bot.message_handler(content_types=['audio'])
+def handle_audio(message):
+  
+  file_id = message.audio.file_id
+  file_name = message.audio.file_name
+  url = file_name.replace(" ", "");
+  file_info = bot.get_file(file_id)
+  print(file_name)
+  file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(API_KEY, file_info.file_path))
+  open(url,'wb').write(file.content)
+  print (url)
+  bot.reply_to(message,'https://replit.com/@MojtabaAzizi/telegramBot#'+url)
+bot.polling()
